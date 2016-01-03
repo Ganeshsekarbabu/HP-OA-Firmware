d90f8a4275fbe2af669d75caace780967d8cec14
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-02 22:35:12 -0800

    Enclosure basic health

---
+++ Find HP Enclosure status.ps1
@@ -0,0 +1,3 @@
+﻿$OA = Get-content (Read-host " Enter the path of the TXT file contains HP OA IPs  ")
+
+connect-HPOA $OA | Get-HPOAEnclosureStatus | Select-Object IP,EnclosureName,StatusMessage,OnboardAdministrator,PowerSubsystem,CoolingSubsystem
