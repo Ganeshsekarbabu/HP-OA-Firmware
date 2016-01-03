06725226e0031c411d5291a3956e270a63250367
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-02 23:23:33 -0800

    Enclosure Health and Stattus

--- Find HP Enclosure status.ps1
+++ Find HP Enclosure status.ps1
@@ -3,7 +3,7 @@
 # Purpose: To find multilple HP Enclosure basic healt and Status ( OnboardAdministrator,Powersubsystem,Coolingsubsystem)
 # Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # By using this HP CMDLTS we can  Configure and manage insight Lights Out (iLO), HP BIOS and Onboard Administrator (OA)
-# To run this script pls check the avilabilty of Get-HPOAFWSummary cmdlet
+# To run this script pls check the avilabilty of Get-HPOAEnclosureStatus cmdlet
 # Reference - http://techbrainblog.com/2016/01/02/powershell-script-to-find-the-basic-health-and-status-of-the-hp-enclosure/
 # Provide the text file path which contains the HP OA IPs
 
