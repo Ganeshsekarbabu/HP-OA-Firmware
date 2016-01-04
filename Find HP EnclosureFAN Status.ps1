4d59ffabd393367106a6c53bd184ca981a693d68
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-03 18:37:13 -0800

    Fan Status

--- Find HP EnclosureFAN Status.ps1
+++ Find HP EnclosureFAN Status.ps1
@@ -4,7 +4,7 @@
 # Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # By using this HP CMDLTS we can  Configure and manage insight Lights Out (iLO), HP BIOS and Onboard Administrator (OA)
 # To run this script pls check the avilabilty of Get-HPOAEnclosureFan cmdlet
-# Reference - 
+# Reference - http://techbrainblog.com/2016/01/03/powershell-script-to-find-the-current-status-of-the-specified-hp-enclosure-fan/
 # Provide the HP OA IP\Name
 
 $OA = Read-host " Enter the HP OA IP or Name "
