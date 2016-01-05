5e8a68412ca918df82c90b04ee197de418ff9a4a
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-04 16:17:16 -0800

    Enclosure Blade Serial Number

--- Find the SerialNumber of baldes in HP OA.ps1
+++ Find the SerialNumber of baldes in HP OA.ps1
@@ -4,7 +4,7 @@
 # Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # By using this HP CMDLTS we can  Configure and manage insight Lights Out (iLO), HP BIOS and Onboard Administrator (OA)
 # To run this script pls check the avilabilty of Get-HPOABladeDeviceSerialNumber cmdlet
-# Reference - 
+# Reference - http://techbrainblog.com/2016/01/04/powershell-script-to-get-the-specified-direct-attached-blade-device-serial-number/
 # Provide the HP OA IP\Name
 
 
