26134c8c77ec4309719e1c11c9f200a1b9738fd8
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-07 22:46:02 -0800

    ilo-firmwareversion

--- HPilo_Firmware_list .ps1
+++ HPilo_Firmware_list .ps1
@@ -3,7 +3,9 @@
 # Download and Install the HP Scripting Tools " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # HPiLOCmdlets-x64 is requried to run this script.
 # We need Find-HPilo and get-HPiLoFirmwareversion cmdlet to run this script.
-# Reference
+#The Get-HPiLOFirmwareVersion cmdlet gets the respective iLO firmware information
+#A list of servers (with or without port number) and corresponding username/password or credential values must be provided as parameters
+# Reference - http://techbrainblog.com/2016/01/07/powershell-script-o-find-the-ilo-firmware-version-with-date/
 
 
  $HPilo = Get-Content ( read-host " Enter the path for the TXT file with ILO IPs " )
