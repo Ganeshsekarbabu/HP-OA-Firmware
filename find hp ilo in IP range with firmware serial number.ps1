1a8857199604d22157dc3af4c3c7bfb0126263df
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-07 21:45:10 -0800

    Find valid ilo in the subnet

--- find hp ilo in IP range with firmware serial number.ps1
+++ find hp ilo in IP range with firmware serial number.ps1
@@ -1,6 +1,6 @@
 ﻿# Author : Ganesh Sekarbabu
 # Site - https://techbrainblog.com/
-# Purpose: To find ilo in the subnet with the Serailnumber,firmware version,blademodel etc
+# Purpose: To find valid ilo's in the subnet with the Serailnumber,firmware version,blademodel etc
 # Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # The Find-HPiLO cmdlet lists valid iLOs in the subnet provided. You must provide the subnet in which the iLOs have to be searched.
 # By default it searchs the default port in the iLOs. You can provide a port number by appending a port number (":#") to the IP range. The port number part is optional.
