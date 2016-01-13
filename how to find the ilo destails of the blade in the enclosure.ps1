a964e819a4b3df591200b7789734aade7275bdc9
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-12 17:09:15 -0800

    ilo details of the blade in enclosure

--- how to find the ilo destails of the blade in the enclosure.ps1
+++ how to find the ilo destails of the blade in the enclosure.ps1
@@ -4,7 +4,7 @@
 # Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # By using this HP CMDLTS we can  Configure and manege insight Lights Out (iLO), HP BIOS and Onboard Administrator (OA)
 # To run this script pls check the avilabilty of Get-HPOAServerList cmdlet
-#Reference - 
+#Reference - https://github.com/Ganeshsekarbabu/HP-OA-Firmware/blob/master/how%20to%20find%20the%20ilo%20destails%20of%20the%20blade%20in%20the%20enclosure.ps1
 # Provide the OA Name\IP
 
 
