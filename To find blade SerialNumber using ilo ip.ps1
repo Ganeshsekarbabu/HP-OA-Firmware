a1292efa264040ad8be743a8108bfd8391c55c67
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-01-12 17:35:08 -0800

    blade serial number using ilo ips

---
+++ To find blade SerialNumber using ilo ip.ps1
@@ -0,0 +1,13 @@
+﻿#Author = Ganesh Sekarbabu
+#Purpose = To get the Serail Number of the HP Blades
+# Download and Install the HP Scripting Tools " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
+# HPiLOCmdlets-x64 is requried to run this script
+# To run this script pls check the avilabilty of Find-HPiLO cmdlet
+#Reference - http://techbrainblog.com/2016/01/04/powershell-script-to-get-the-specified-direct-attached-blade-device-serial-number/
+
+$HPilo = Get-Content ( read-host " Enter the path for the TXT file with ILO IPs " )
+
+$HPilo | foreach { (Find-HPiLO $_) } | Select-Object IP,PN,SerialNumber |
+
+Export-Csv  (read-host " Enter the path to save the result " )
+
