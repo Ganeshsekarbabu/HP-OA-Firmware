df585f3615a7ae5eba1a31607456b4c6697478b3
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-02-10 15:00:51 -0800

    To update the Onboard Administrator firmware

---
+++ update-firmware-onboardadministrator.ps1
@@ -0,0 +1,20 @@
+﻿# Author : Ganesh Sekarbabu
+# Site - https://techbrainblog.com/
+# Purpose: To update the Onboard Administrator firmware.
+# Download the HP CMDLTS from the " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
+# To run this script pls check the avilabilty of Update-HPOAFirmware
+# Reference - 
+# Provide the ftp path which contains the firmware bin
+
+
+
+$OA =  Read-host " Enter the HP OA IP "
+
+
+$c = Connect-HPOA $OA
+
+$url = Read-Host " Enter the FTP path of the firmware (ftp://IP-ADD/*.bin) "
+
+Update-HPOAFirmware -Connection $c -Source URL -url $url
+
+Disconnect-HPOA $c
