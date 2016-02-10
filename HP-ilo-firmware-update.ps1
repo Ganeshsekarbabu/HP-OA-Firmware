621d541e493e4ed6da5aece0aa7e471a902e9a2f
Ganeshsekarbabu <sganesh79@hotmail.com>
2016-02-10 14:35:25 -0800

    To update the HPilofirmware from the list of IPs

--- HP-ilo-firmware-update.ps1
+++ HP-ilo-firmware-update.ps1
@@ -3,6 +3,7 @@
 #Purpose = To update the HPilofirmware from the list of IPs
 # Download and Install the HP Scripting Tools " http://www8.hp.com/us/en/products/server-software/product-detail.html?oid=5440657#!tab=features "
 # HPiLOCmdlets-x64 is requried to run this script
+# Reference - : http://techbrainblog.com/2016/02/10/powershell-script-to-update-the-ilo-firmware-from-the-list-of-ips/
 #Provide the IPs in the text file
 # Download the firmware and provide the path of the firmware.
 
